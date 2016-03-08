3e018a82868126ca4c45471821bad31203045657
pikesley <github@orgraphone.org>
2016-03-08 10:55:18 +0000

    This might be about to get awesome

--- spec/spindle_gouge/spindle_gouge_spec.rb
+++ spec/spindle_gouge/spindle_gouge_spec.rb
@@ -10,7 +10,7 @@ module SpindleGouge
 
     context 'simple SVG' do
       it 'serves up a SVG' do
-        get '/logo/print/basic', nil, SVG_HEADERS
+        get '/logo/web/basic', nil, SVG_HEADERS
         expect(last_response.headers['Content-type']).to eq 'image/svg+xml'
         expect(last_response.headers['Content-length']).to eq '28795'
       end
@@ -18,13 +18,13 @@ module SpindleGouge
 
     context 'PNGs' do
       it 'serves up a PNG' do
-        get '/logo/print/basic', nil, PNG_HEADERS
+        get '/logo/web/basic', nil, PNG_HEADERS
         expect(last_response.headers['Content-type']).to eq 'image/png'
         expect(last_response.headers['Content-length']).to be >= '2694'
       end
 
       it 'scales a PNG' do
-        get '/logo/print/basic?width=100', nil, PNG_HEADERS
+        get '/logo/web/basic?width=100', nil, PNG_HEADERS
         expect(last_response.headers['Content-type']).to eq 'image/png'
         expect(last_response.headers['Content-length']).to be <= '1876'
       end
